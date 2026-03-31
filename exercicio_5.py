81e749fa63506026295a9fa2e4b2b9edfde3b58b
heloisacastralli <heloisa.castralli@gmail.com>
2026-03-30 21:28:49 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -55,3 +55,16 @@ Resposta:
 Não
 
 """
+
+cromossomo = input("Digite o cromossomo da variante: ")
+posicao = int(input("Digite a posição da variante: "))
+genoma = input("Digite o genoma de referência: ")
+
+if cromossomo == "chr17" and 41196312 <= posicao <= 41277500 and genoma == "hg19":
+	print("Sim")
+
+else:
+	if cromossomo == "chr17" and 43044295 <= posicao <= 43125483 and genoma == "hg38":
+		print("Sim")
+	else:
+		print("Não")
